b4a662806f64376f00c333260cac739bdac8f824
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2019-02-21 15:19:11 +0700

    First commit

--- src/app/core/interceptors/token.interceptor.ts
+++ src/app/core/interceptors/token.interceptor.ts
@@ -12,8 +12,7 @@ import { Observable } from 'rxjs/Observable';
 export class TokenInterceptor implements HttpInterceptor {
   constructor() {}
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    
-    // request = request.clone({
+ // request = request.clone({
     //   setHeaders: {
     //     Authorization: `Bearer ${this.auth.getToken()}`
     //   }
